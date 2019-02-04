04a6cefb89aad1a47309888c216f8f386be36267
Ikey Benzaken <ikey.benz@gmail.com>
2019-02-04 11:48:01 -0800

    Added support for auth middleware, cookie parser, and public folder

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
+app.use(require('cookie-parser')());
+app.use(express.static('public'));
+
+// This will check for an authenticated user before every request
+app.use(require('./middleware/auth'));
 
 app.get('/', (req, res) => {
     res.render('index');
